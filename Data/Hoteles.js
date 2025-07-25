ceb77d6f068f801e2a25d3bde9ebcebfac008639
Alejandro <alejbautistaa@gmail.com>
2025-07-25 01:23:45 -0600

    Carpeta modal creada

---
+++ Data/Hoteles.js
@@ -0,0 +1,102 @@
+export const Hoteles = [
+    {
+    nombre: 'Hotel Santa Fe',
+    direccion: 'Calle Principal #123, Paracho',
+    descripcion: 'Hotel céntrico con desayuno incluido y Wi-Fi.',
+    imagen: '/imagenes/hoteles/paracho1.jpg',
+    precioDesde: 800,
+    puebloMagico: 'paracho',
+    habitaciones: [
+        { tipo: 'Estándar', precio: 800, detalles: '1 cama matrimonial, baño privado' },
+        { tipo: 'Suite', precio: 1200, detalles: '1 cama king, desayuno incluido' }
+    ]
+    },
+    {
+    nombre: 'Hotel Colonial Cuitzeo',
+    direccion: 'Av. Morelos #45, Centro, Cuitzeo',
+    descripcion: 'Arquitectura colonial y excelente ubicación frente al lago.',
+    imagen: '/imagenes/hoteles/cuitzeo1.jpg',
+    precioDesde: 700,
+    puebloMagico: 'cuitzeo',
+    habitaciones: [
+        { tipo: 'Estándar', precio: 700, detalles: '1 cama matrimonial, baño privado' }
+    ]
+    },
+    {
+    nombre: 'Hotel La Casona',
+    direccion: 'Calle Hidalgo #67, Pátzcuaro',
+    descripcion: 'Estancia tradicional con patio interior y desayuno típico.',
+    imagen: '/imagenes/hoteles/patzcuaro1.jpg',
+    precioDesde: 900,
+    puebloMagico: 'patzcuaro',
+    habitaciones: [
+        { tipo: 'Estándar', precio: 900, detalles: 'Cama doble, Wi-Fi gratuito' }
+    ]
+    },
+    {
+    nombre: 'Hotel Pueblo Dorado',
+    direccion: 'Centro histórico, Tlalpujahua',
+    descripcion: 'Vistas increíbles a las montañas y decoración rústica.',
+    imagen: '/imagenes/hoteles/tlalpujahua1.jpg',
+    precioDesde: 850,
+    puebloMagico: 'tlalpujahua',
+    habitaciones: [
+        { tipo: 'Estándar', precio: 850, detalles: '1 cama queen, calefacción' }
+    ]
+    },
+    {
+    nombre: 'Hotel Santa Clara Colonial',
+    direccion: 'Plaza Principal #22, Santa Clara del Cobre',
+    descripcion: 'Ambiente tradicional, cerca de talleres de cobre.',
+    imagen: '/imagenes/hoteles/santa-clara1.jpg',
+    precioDesde: 750,
+    puebloMagico: 'santa-clara-del-cobre',
+    habitaciones: [
+        { tipo: 'Estándar', precio: 750, detalles: 'Cama doble, baño privado' }
+    ]
+    },
+    {
+    nombre: 'Hotel El Oasis',
+    direccion: 'Avenida Reforma #10, Tacámbaro',
+    descripcion: 'Con alberca, jardines y desayuno incluido.',
+    imagen: '/imagenes/hoteles/tacambaro1.jpg',
+    precioDesde: 880,
+    puebloMagico: 'tacambaro',
+    habitaciones: [
+        { tipo: 'Estándar', precio: 880, detalles: '1 cama matrimonial, Wi-Fi, aire acondicionado' }
+    ]
+    },
+    {
+    nombre: 'Hotel Los Azufres Inn',
+    direccion: 'Zona de los Azufres, Jungapeo',
+    descripcion: 'Cabañas junto a aguas termales, ideal para relajarse.',
+    imagen: '/imagenes/hoteles/jungapeo1.jpg',
+    precioDesde: 1100,
+    puebloMagico: 'jungapeo',
+    habitaciones: [
+        { tipo: 'Cabaña', precio: 1100, detalles: 'Cama matrimonial, chimenea' }
+    ]
+    },
+    {
+    nombre: 'Hotel Vista Lago',
+    direccion: 'Orilla del lago, Jiquilpan',
+    descripcion: 'Vistas espectaculares al lago y entorno tranquilo.',
+    imagen: '/imagenes/hoteles/jiquilpan1.jpg',
+    precioDesde: 780,
+    puebloMagico: 'jiquilpan',
+    habitaciones: [
+        { tipo: 'Estándar', precio: 780, detalles: 'Cama doble, balcón con vista al lago' }
+    ]
+    },
+    {
+    nombre: 'Hotel Mineral',
+    direccion: 'Centro, Angangueo',
+    descripcion: 'Hotel con historia minera, cerca del santuario de mariposas.',
+    imagen: '/imagenes/hoteles/angangueo1.jpg',
+    precioDesde: 820,
+    puebloMagico: 'angangueo',
+    habitaciones: [
+        { tipo: 'Estándar', precio: 820, detalles: 'Cama matrimonial, desayuno incluido' }
+    ]
+    }
+];
---
+++ Data/pueblosMagicos.js
@@ -0,0 +1,52 @@
+export const pueblosMagicos = [
+    {
+    nombre: 'Angangueo',
+    slug: 'angangueo',
+    icono: '/imagenes/puebloMagico/puebloM8.svg',
+    },
+    {
+    nombre: 'Cuitzeo',
+    slug: 'cuitzeo',
+    icono: '/imagenes/puebloMagico/puebloM2.svg',
+    },
+    {
+    nombre: 'Jiquilpan',
+    slug: 'jiquilpan',
+    icono: '/imagenes/puebloMagico/puebloM9.svg',
+    },
+    {
+    nombre: 'Paracho',
+    slug: 'paracho',
+    icono: '/imagenes/puebloMagico/puebloM6.svg',
+    },
+    {
+    nombre: 'Patzcuaro',
+    slug: 'patzcuaro',
+    icono: '/imagenes/puebloMagico/puebloM4.svg',
+    },
+    {
+    nombre: 'Santa Clara del Cobre',
+    slug: 'santa-clara-del-cobre',
+    icono: '/imagenes/puebloMagico/puebloM7.svg',
+    },
+    {
+    nombre: 'Tacambaro',
+    slug: 'tacambaro',
+    icono: '/imagenes/puebloMagico/puebloM3.svg',
+    },
+    {
+    nombre: 'Tlalpujahua',
+    slug: 'tlalpujahua',
+    icono: '/imagenes/puebloMagico/puebloM5.svg',
+    },
+    {
+        nombre: 'san-jose',
+        slug: 'san-jose',
+        icono: '/imagenes/puebloMagico/puebloM10.svg',
+    },
+    {
+    nombre: 'Tzintzuntzan',
+    slug: 'tzintzuntzan',
+    icono: '/imagenes/puebloMagico/puebloM1.svg',
+    }
+]
